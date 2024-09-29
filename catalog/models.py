d9a5c04ada08055f24956bc623c37c68b3e553aa
Albert <autelec@yandex.ru>
2024-09-29 16:44:53 +0400

    mgration 0001

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название продукта")
+    discription = models.TextField(
+        verbose_name="Описание продукта", blank=True, null=True
+    )
+    image = models.ImageField(
+        upload_to="products/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение продукта",
+    )
+    category = models.ForeignKey('Category',
+        on_delete=models.SET_NULL, verbose_name="Категория", blank=True, null=True,
+        related_name='products',
+    )
+    price = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    update_at = models.DateField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название категории")
+    discription = models.TextField(
+        verbose_name="Описание категории", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+# наименование,
+# описание,
+# изображение,
+# категория,
+# цена за покупку,
+# дата создания,
+# дата последнего изменения.
