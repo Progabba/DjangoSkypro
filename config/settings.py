fad3bf4b41c4b33e75b62f8acd9010bae46bc736
Albert <autelec@yandex.ru>
2024-10-09 19:20:46 +0400

    add include to base, add my_tag for images

--- config/settings.py
+++ config/settings.py
@@ -132,5 +132,5 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "meida")
